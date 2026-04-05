1d2f5fbac1e9f81071f698b8e1f5f3cd731ee342
ana_lehova <lehoa02@uwgb.edu>
2026-04-04 21:23:21 -0500

    hosting changer

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,7 @@ const app = express();
 app.use(cors({
     origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
 
